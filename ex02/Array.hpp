511280275ec79b5b7bd8c75748e8382c50926683
Juiceee <lbehr@student.42.fr>
2024-08-02 13:39:29 +0200

    ca doit etre plutot good

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -15,7 +15,15 @@ class Array
 		Array(unsigned int n);
 		Array(const Array<T> &obj);
 		Array &operator=(const Array &obj);
+		T &operator[](int size);
 		~Array();
+		int	size();
+
+		class indexOutOfBound : public std::exception
+		{
+		public:
+			virtual const char *what() const throw();
+		};
 };
 
 template <typename T>
@@ -40,23 +48,48 @@ template <typename T>
 Array<T>::Array(const Array<T> &obj)
 {
 	std::cout << "Copy constructor called\n";
-	*this = obj;
+	this->_size = obj._size;
+	this->_array = new T[this->_size];
+	for (int i = 0; i < this->_size; i++)
+		this->_array[i] = obj._array[i];
 }
 
 template <typename T>
 Array<T> &Array<T>::operator=(const Array<T> &obj)
 {
 	std::cout << "Copy assignment operator called\n";
-	if (this != &obj)
-	{
-		
-	}
+	this->_size = obj._size;
+	this->_array = new T[this->_size];
+	for (int i = 0; i < this->_size; i++)
+		this->_array[i] = obj._array[i];
 	return *this;
 }
+
+template <typename T>
+T &Array<T>::operator[](int size)
+{
+	if (size < 0 || size >= this->_size)
+		throw (indexOutOfBound());
+	return (this->_array[size]);
+}
+
+template <typename T>
+const char *Array<T>::indexOutOfBound::what() const throw()
+{
+	return ("Index Out Of Bound\n");
+}
+
+template <typename T>
+int	Array<T>::size()
+{
+	return (this->_size);
+}
+
 
 template <typename T>
 Array<T>::~Array()
 {
 	std::cout << "Destructor of Array called\n";
+	delete [] _array;
 }
 #endif
